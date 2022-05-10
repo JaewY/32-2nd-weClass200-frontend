a00588442e83b72bea72d7cec2c7979740689ef4
u-rim <99340550+u-rim@users.noreply.github.com>
2022-05-10 19:29:40 +0900

    로그인 기능 구현 완료

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
-const example = "hi";
+export const REDIRECT_URI = "http://localhost:3000/kakaoauth";
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+export const API_URL = "http://10.58.3.151:8000/users/kakao";
